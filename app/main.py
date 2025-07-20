1b5c259ced91fda134fb416e9315bfe07dea193a
Sameet Patil <sameetpatil5@gmail.com>
2025-07-20 21:45:56 +0530

    Add Prometheus setup

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 # app/main.py
 
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.routes import router
 
+# FastAPI setup
 app = FastAPI(title="Paddy: PDF Tools API")
 
+# Instrumentator setup
+Instrumentator().instrument(app).expose(app)
+
+# Include the router
 app.include_router(router)
